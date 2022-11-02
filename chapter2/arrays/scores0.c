90ffe7bc8f90dc4842646fe309feffc136db6a5a
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-11-02 15:03:32 +0000

    Updated 11/2

---
+++ chapter2/arrays/scores0.c
@@ -0,0 +1,16 @@
+// Average three numbers (hardcoded) using an array
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Scores
+    int scores[3];
+    scores[0] = 63;
+    scores[1] = 42;
+    scores[2] = 98;
+
+    // Print the average
+    printf("Average: %0.1f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+}
---
+++ chapter2/arrays/scores3.c
@@ -0,0 +1,30 @@
+// Average numbers using an array
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Let the user decide how many scores to enter
+    int n = get_int("How many scores do you want to enter? ");
+
+    // Scores
+    int scores[n];
+    for (int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+
+    // Print the average
+    printf("Average: %0.1f\n",
+}
+
+float average(int length, int array[])
+{
+    // Calculate the sum
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+}
