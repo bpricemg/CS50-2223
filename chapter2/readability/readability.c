fa1af2d3cde4bf8e78ffd9c7591ab6ff683485e6
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-11-10 15:31:18 +0000

    Updated 11/10

---
+++ chapter2/readability/readability.c
@@ -0,0 +1,39 @@
+// Computes the approximate grade level needed to comprehend some text
+
+
+int main(void)
+{
+    // Ask the user for some text and save it
+
+    // Call a function to count the number of letters
+
+    // Call a function to count the number of words
+
+    // Call a function to count the number of sentences
+
+    // Calulate the Coleman-Liau index
+
+    // Print the reading level
+}
+
+/**
+ * A function that returns the number of letter
+*/
+int count_letters(string s)
+{
+    // Initialize a counter
+
+    // Loop through the text
+        // If a letter (isalpha may be helpful here)
+            // Add 1 to the counter
+
+    // Return the value of the counter/the number of letters
+}
+
+/**
+ * A function that returns the number of words
+*/
+
+/**
+ * A function that returns the number of sentences
+*/
