9481336d9c90d465ad63a1fba4e6fcaadc929649
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-01-30 09:43:06 -0500

    Delete

--- chapter4/pointers/address3.c
+++
@@ -1,9 +0,0 @@
-// Store and print an integers address
-#include <stdio.h>
-
-int main(void)
-{
-    int n = 50;
-    int *p = &n;
-    printf("%p\n", p);
-}
--- chapter4/pointers/student.h
+++
@@ -1,6 +0,0 @@
-typedef struct
-{
-    string name;
-    float gpa;
-}
-student;
