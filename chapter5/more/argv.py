3add65a167d042730fd20b45171c207d174deb2f
bpricemg <43038187+bpricemg@users.noreply.github.com>
2023-02-15 11:18:20 -0500

    Updated 2/15

--- chapter5/more/argv.py
+++ chapter5/more/argv.py
@@ -1,7 +1,10 @@
+# Say hello to a user given their name in command line
 from sys import argv
 
+# If there is at least one name
 if len(argv) >= 2:
     print("hello, ", end ="")
+    # Print every name after argv[0] (which is the program name)
     for arg in argv[1:]:
         print(f"{arg} ", end="")
     print()
--- chapter5/more/string3.py
+++ chapter5/more/string3.py
@@ -4,10 +4,13 @@ name = input("Full name: ")
 # Start a counter
 names = 1
 
+# List of characters that could mean a new name
 special_char = [" ", "-"]
 
 # Look for spaces
 for c in name:
     if c in special_char:
         names += 1
+
+# Print number of names
 print(f"You have {names} names.")
